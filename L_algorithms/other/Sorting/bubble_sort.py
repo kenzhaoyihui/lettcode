35d20089a8f2bccb8cff2ea6d750d16d60a800ca
yzhao <2993614148@qq.com>
2017-11-02 20:04:36 +0800

    add other/sorting

---
+++ L_algorithms/other/Sorting/bubble_sort.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+__author__ = "kenzhaoyihui"
+
+
+def bubble_sort(lists):
+    # 冒泡排序
+    count = len(lists)
+    for i in range(0, count):
+        for j in range(i + 1, count):
+            if lists[i] > lists[j]:
+                lists[i], lists[j] = lists[j], lists[i]
+    return lists
